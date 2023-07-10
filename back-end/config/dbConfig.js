5a53278c93dff8f50fe96d2793105f0e9a93baae
mahianbes12 <88880755+mahianbes12@users.noreply.github.com>
2023-07-10 15:16:03 -0400

    user login and auth

--- back-end/config/dbConfig.js
+++ back-end/config/dbConfig.js
@@ -1,8 +1,8 @@
 module.exports = {
     HOST: 'localhost',
     USER: 'root',
-    PASSWORD: '123456789',
-    DB: 'epayment',
+    PASSWORD: '12345678',
+    DB: 'payment',
     dialect: 'mysql',
     pool: {
       max: 5,
