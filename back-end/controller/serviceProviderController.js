22076f772aea0195f09f66491cd6848c7bdc2659
mahianbes12 <88880755+mahianbes12@users.noreply.github.com>
2023-07-14 23:59:30 -0400

    Service provider Controller

--- back-end/controller/serviceProviderController.js
+++ back-end/controller/serviceProviderController.js
@@ -1,175 +1,434 @@
 const asyncHandler = require('express-async-handler');
 const db = require('../models');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-require('dotenv').config();
-const multer = require('multer')
-const path = require ('path')
+const { Op } = require('sequelize');
+const multer = require('multer');
+const path = require('path');
 
-const ServiceProviders = db.ServiceProviders;
+const fs = require('fs');
+const { Agents, ServiceProviders, User, Payment, Bill } = require('../models');
 
-// Create and save a new service provider
 exports.create = asyncHandler(async (req, res) => {
-  // Validate request
+  // validate
+  const requiredFields = [
+    'serviceProviderBIN',
+    'serviceProviderName',
+    'servicesOffered',
+    'BankName',
+    'BankAccountNumber',
+    'phoneNumber',
+  ];
 
-  if (
-    !req.body.serviceProviderBIN ||
-    !req.body.serviceProviderName ||
-    !req.body.serviceProviderEmail ||
-    !req.body.serviceProviderPassword ||
-    !req.body.servicesOffered ||
-    !req.body.BankName ||
-    !req.body.BankAccountNumber ||
-    !req.body.phoneNumber ||
-    !req.file.path
+  const missingFields = requiredFields.filter((field) => !req.body[field]);
 
-  ) {
-    res.status(400).send({
-      message:
-        'serviceProviderBIN, serviceProviderName, serviceProviderEmail, serviceProviderPassword, servicesOffered, BankName, BankAccountNumber, phoneNumber, and serviceProviderAuthorizationLetter cannot be empty',
-    });
-    return; 
+  // Check if serviceProviderAuthorizationLetter field exists
+  if (!req.file || !req.file.path) {
+    missingFields.push('serviceProviderAuthorizationLetter');
   }
-  //Check if service provider already exists
-  const existingServiceProvider= await ServiceProviders.findOne({
-    where: {
-      serviceProviderEmail: req.body.serviceProviderEmail,
-    },
-  });
 
-  if (existingServiceProvider) {
-    res.status(409).send({
-      message: 'Service provider already exists',
+  if (missingFields.length > 0) {
+    res.status(400).send({
+      message: `${missingFields.join(', ')} cannot be empty`,
     });
     return;
   }
 
-  // Hash the password
-  const hashedPassword = await bcrypt.hash(req.body.serviceProviderPassword, 10);
-  // Create a service provider object
-  const services = {
-    serviceProviderBIN: req.body.serviceProviderBIN,
-    serviceProviderName: req.body.serviceProviderName,
-    serviceProviderEmail: req.body.serviceProviderEmail,
-    serviceProviderPassword:hashedPassword,
-    servicesOffered: req.body.servicesOffered,
-    BankName: req.body.BankName,
-    BankAccountNumber: req.body.BankAccountNumber,
-    phoneNumber: req.body.phoneNumber,
-    serviceProviderAuthorizationLetter: req.file.path,
-
-  };
-
-  // Save service provider in the database
-  const data = await ServiceProviders.create(services);
-  res.send(data);
-});
-
-// Retrieve all service providers from the database
-exports.findAll = asyncHandler(async (req, res) => {
-  const data = await ServiceProviders.findAll();
-  res.send(data);
-});
-
-// Find a single service provider by id
-exports.findOne = asyncHandler(async (req, res) => {
-  const id = req.params.id;
-
-  const data = await ServiceProviders.findByPk(id);
-  if (!data) {
-    res.status(404).send({
-      message: `ServiceProviders with id=${id} not found`,
-    });
-  } else {
-    res.send(data);
-  }
-});
-
-// Update a service provider by id
-exports.update = asyncHandler(async (req, res) => {
-  const id = req.params.id;
-
-  const [num] = await ServiceProviders.update(req.body, {
-    where: { id: id },
-  });
-
-  if (num === 1) {
-    res.send({
-      message: 'ServiceProviders was updated successfully.',
-    });
-  } else {
-    res.send({
-      message: `Cannot update ServiceProviders with id=${id}. ServiceProviders not found or req.body is empty!`,
-    });
-  }
-});
-
-// Delete a service provider by id
-exports.delete = asyncHandler(async (req, res) => {
-  const id = req.params.id;
-
-  const num = await ServiceProviders.destroy({
-    where: { id: id },
-  });
-
-  if (num === 1) {
-    res.send({
-      message: 'ServiceProviders was deleted successfully!',
-    });
-  } else {
-    res.send({
-      message: `Cannot delete ServiceProviders with id=${id}. ServiceProviders not found!`,
-    });
-  }
-});
-
-// Service provider login auth
-exports.login = asyncHandler(async (req, res) => {
   try {
-    const { serviceProviderEmail, serviceProviderPassword } = req.body;
-    const serviceProvider = await ServiceProviders.findOne({
-      where: { serviceProviderEmail },
+    const {
+      serviceProviderBIN,
+      serviceProviderName,
+      servicesOffered,
+      BankName,
+      BankAccountNumber,
+      phoneNumber,
+    } = req.body;
+
+    // Check if service provider already exists
+    const existingServiceProvider = await ServiceProviders.findOne({
+      where: {
+        serviceProviderBIN: serviceProviderBIN,
+      },
+    });
+
+    if (existingServiceProvider) {
+      return res.status(409).json({
+        error: 'Service provider already exists',
+      });
+    }
+
+    // Check if serviceProviderAuthorizationLetter field exists
+    if (!req.file || !req.file.path) {
+      return res.status(400).json({
+        error: 'Service provider authorization letter is required',
+      });
+    }
+
+    // Create the service provider
+    // Create the service provider
+const serviceProvider = await ServiceProviders.create({
+  serviceProviderBIN,
+  serviceProviderName,
+  servicesOffered,
+  BankName,
+  BankAccountNumber,
+  phoneNumber,
+  serviceProviderAuthorizationLetter: req.file.path,
+});
+
+// Find the existing agent by agentId
+if(req.body.agentBIN){
+const agentBIN = req.body.agentBIN; 
+const agent = await Agents.findByPk(agentBIN);
+if (!agent) {
+  return res.status(404).json({
+    error: 'Agent not found',
+  });
+}
+
+await serviceProvider.addAgents(agent);
+}
+if(req.body.UserID){
+  const UserID = req.body.UserID; 
+  const user = await User.findByPk(UserID);
+  if (!user) {
+    return res.status(404).json({
+      error: 'User not found',
+    });
+  }
+  
+  await serviceProvider.addUsers(user);
+  }
+
+// Find the existing bill by billId
+if(req.body.billId){
+  const billId = req.body.billId; // Assuming the billId is provided in the request body
+const bill = await Bill.findByPk(billId);
+if (!bill) {
+  return res.status(404).json({
+    error: 'Bill not found',
+  });
+}
+
+await serviceProvider.addBills(bill);
+}
+
+if(req.body.PaymentId){
+  const PaymentId = req.body.PaymentId; // Assuming the billId is provided in the request body
+const payment = await Payment.findByPk(PaymentId);
+if (!Payment) {
+  return res.status(404).json({
+    error: 'Payment not found',
+  });
+}
+
+await serviceProvider.addPayments(payment);
+}
+
+return res.send({
+  message: 'Service provider was created successfully.',
+serviceProvider,});
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      error: 'Server error',
+    });
+  }
+});
+
+// Update a service provider
+exports.update = asyncHandler(async (req, res) => {
+  try {
+    const serviceProviderId = req.params.id;
+    const {
+      serviceProviderBIN,
+      serviceProviderName,
+      servicesOffered,
+      BankName,
+      BankAccountNumber,
+      phoneNumber,
+    } = req.body;
+
+    const serviceProvider = await ServiceProviders.findByPk(serviceProviderId);
+    if (!serviceProvider) {
+      return res.status(404).json({
+        error: 'Service provider not found',
+      });
+    }
+
+    serviceProvider.serviceProviderBIN = serviceProviderBIN;
+    serviceProvider.serviceProviderName = serviceProviderName;
+    serviceProvider.servicesOffered = servicesOffered;
+    serviceProvider.BankName = BankName;
+    serviceProvider.BankAccountNumber = BankAccountNumber;
+    serviceProvider.phoneNumber = phoneNumber;
+
+    // Check if a new file is uploaded
+    if (req.file && req.file.path) {
+      // Delete the previous authorization letter file
+      if (serviceProvider.serviceProviderAuthorizationLetter) {
+        fs.unlinkSync(serviceProvider.serviceProviderAuthorizationLetter);
+      }
+      serviceProvider.serviceProviderAuthorizationLetter = req.file.path;
+    }
+
+    await serviceProvider.save();
+    
+    // Update the associations with the agent and the bill
+    if (req.body.agentBINs && req.body.agentBINs.length > 0) {
+      const agents = await Agents.findAll({
+        where: {
+          id: {
+            [Op.in]: req.body.agentBINs,
+          },
+        },
+      });
+      if (!agents) {
+        return res.status(404).json({
+          error: 'Agent not found',
+        });
+      }
+
+      // Associate the service provider with the bill
+    serviceProvider.agentBIN = agents.agentBIN;
+    }
+    if (req.body.billIds && req.body.billIds.length > 0) {
+      const bill = await Bill.findAll({
+        where: {
+          id: {
+            [Op.in]: req.body.billIds,
+          },
+        },
+      });
+      if (!bill) {
+        return res.status(404).json({
+          error: 'Agent not found',
+        });
+      }
+
+      // Associate the service provider with the bill
+    serviceProvider.billId = bill.billId;
+    }
+    if (req.body.UserIds && req.body.UserIds.length > 0) {
+      const user = await User.findAll({
+        where: {
+          id: {
+            [Op.in]: req.body.UserIds,
+          },
+        },
+      });
+      if (!user) {
+        return res.status(404).json({
+          error: 'user not found',
+        });
+      }
+
+    serviceProvider.UserID = user.UserID;
+    }
+    if (req.body.PaymentIds && req.body.PaymentIds.length > 0) {
+      const payment = await Payment.findAll({
+        where: {
+          id: {
+            [Op.in]: req.body.PaymentIds,
+          },
+        },
+      });
+      if (!payment) {
+        return res.status(404).json({
+          error: 'Payment not found',
+        });
+      }
+
+      // Associate the service provider with the bill
+    serviceProvider.PaymentId = payment.PaymentId;
+    }
+
+
+    // Remove associations with payment, bill, and agents
+    if (req.body.removePaymentIds && req.body.removePaymentIds.length > 0) {
+      const payment = await Payment.findAll({
+        where: {
+          id: {
+            [Op.in]: req.body.removePaymentIds,
+          },
+        },
+      });
+      if (payment) {
+        await serviceProvider.removepayment(payment);
+      }
+    }
+
+    if (req.body.removeUserIds && req.body.removeUserIds.length > 0) {
+      const user = await User.findAll({
+        where: {
+          id: {
+            [Op.in]: req.body.removeUserIds,
+          },
+        },
+      });
+      if (user) {
+        await serviceProvider.removeUser(user);
+      }
+    }
+
+  if (req.body.removeBillIds && req.body.removeBillIds.length > 0) {
+    const bills = await Bill.findAll({
+      where: {
+        id: {
+          [Op.in]: req.body.removeBillIds,
+        },
+      },
+    });
+    if (bills) {
+      await serviceProvider.removeBill(bills);
+    }
+  }
+
+  if (req.body.removeAgentIds && req.body.removeAgentIds.length > 0) {
+    const agents = await Agents.findAll({
+      where: {
+        id: {
+          [Op.in]: req.body.removeAgentIds,
+        },
+      },
+    });
+    if (agents) {
+      await serviceProvider.removeAgents(agents);
+    }
+  }
+
+  // Save the updated bill
+  await serviceProvider.save();
+
+
+    return res.status(200).json({
+      message: 'Service provider was updated successfully.',
+      serviceProvider,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      error: 'Server error',
+    });
+  }
+});
+
+
+// Get all service providers
+exports.findAll = asyncHandler(async (req, res) => {
+  try {
+    const serviceProviders = await ServiceProviders.findAll({
+      include: [
+        {
+          model: Agents,
+          as: 'Agents',
+        },
+        {
+          model: Bill,
+          as: 'Bills',
+        },
+        {
+          model: User,
+          as: 'User',
+        },
+        {
+          model: Payment,
+          as: 'Payments',
+        },
+      ],
+    });
+
+    return res.send(serviceProviders);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      error: 'Server error',
+    });
+  }
+});
+
+// Get service provider by ID
+exports.findOne = asyncHandler(async (req, res) => {
+  try {
+    const serviceProviderId = req.params.id;
+
+    const serviceProvider = await ServiceProviders.findByPk(serviceProviderId, {
+      include: [
+        {
+          model: Agents,
+          as: 'Agents',
+        },
+        {
+          model: Bill,
+          as: 'Bills',
+        },
+        {
+          model: User,
+          as: 'User',
+        },
+        {
+          model: Payment,
+          as: 'Payments',
+        },
+      ],
     });
 
     if (!serviceProvider) {
-      res.status(404).json({ error: 'Service provider not found' });
-    } else {
-      const passwordMatch = await bcrypt.compare(
-        serviceProviderPassword,
-        serviceProvider.serviceProviderPassword
-      );
-
-      if (!passwordMatch) {
-        res.status(401).json({ error: 'Incorrect password' });
-      } else {
-        const token = jwt.sign(
-          { serviceProviderBIN: serviceProvider.id },
-          process.env.TOKEN_SECRET,
-          {
-            expiresIn: '1h',
-          }
-        );
-        res.status(200).json({ message: 'Login successful', token });
-      }
+      return res.status(404).json({
+        error: 'Service provider not found',
+      });
     }
+
+    return res.status(200).json({
+      serviceProvider,
+    });
   } catch (error) {
-    res
-      .status(500)
-      .json({ error: 'Failed to login service provider', message: error.message });
+    console.error(error);
+    return res.status(500).json({
+      error: 'Server error',
+    });
   }
 });
 
+// Delete a service provider
+exports.delete = asyncHandler(async (req, res) => {
+  try {
+    const serviceProviderId = req.params.id;
 
-//upload image
-const storage =multer.diskStorage({
-  destination:(req,file,cb)=>{
-    cb(null,'Images')
-  }, 
-  filename :(req, file, cb)=>{
-     cb(null, Date.now()+path.extname(file.originalname))
-      
+    const serviceProvider = await ServiceProviders.findByPk(serviceProviderId);
+
+    if (!serviceProvider) {
+      return res.status(404).json({
+        error: 'Service provider not found',
+      });
+    }
+
+    // Delete the authorization letter file
+    if (serviceProvider.serviceProviderAuthorizationLetter) {
+      fs.unlinkSync(serviceProvider.serviceProviderAuthorizationLetter);
+    }
+
+    await serviceProvider.destroy();
+
+    return res.status(200).json({
+      message: 'Service provider deleted successfully',
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      error: 'Server error',
+    });
   }
-})
+});
+
+// Upload image
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'Images');
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
+});
+
 exports.upload = multer({
   storage: storage,
   limits: { fileSize: '1000000' },
@@ -180,6 +439,6 @@ exports.upload = multer({
     if (mimeType && extname) {
       return cb(null, true);
     }
-    cb('provide the proper format');
-  }
+    cb('Provide the proper format');
+  },
 }).single('serviceProviderAuthorizationLetter');
