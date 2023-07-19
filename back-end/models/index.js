7d519032e7f6a2820f04fbe08ee246de38740f8b
mahianbes12 <88880755+mahianbes12@users.noreply.github.com>
2023-07-19 00:45:23 -0400

    Modelsl

--- back-end/models/index.js
+++ back-end/models/index.js
@@ -1,43 +1,104 @@
-'use strict';
+"use strict";
+const { Model, Sequelize } = require('sequelize');
+const dbConfig = require('../config/dbConfig.js');
 
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
-const process = require('process');
-const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const sequelize = new Sequelize(
+  dbConfig.DB,
+  dbConfig.USER,
+  dbConfig.PASSWORD, {
+    host: dbConfig.HOST,
+    dialect: dbConfig.dialect,
+    operatorsAliases: false,
+    pool: {
+      max: dbConfig.pool.max,
+      min: dbConfig.pool.min,
+      acquire: dbConfig.pool.acquire,
+      idle: dbConfig.pool.idle
+    }
+  }
+);
+
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
 
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (
-      file.indexOf('.') !== 0 &&
-      file !== basename &&
-      file.slice(-3) === '.js' &&
-      file.indexOf('.test.js') === -1
-    );
-  })
-  .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-    db[model.name] = model;
-  });
+db.Bill = require('./billModel.js')(sequelize, Sequelize);
+db.Agents = require('./agentModel.js')(sequelize, Sequelize);
+db.User = require('./UserModel.js')(sequelize, Sequelize);
+db.ServiceProviders = require('./ServiceProviderModel.js')(sequelize, Sequelize);
+db.Payment = require('./PaymentModel.js')(sequelize, Sequelize);
+// db.AgentHistory = require('./agentHistoryModel.js')(sequelize, Sequelize);
+// db.ServiceProviderHistory = require('./serviceProviderHistoryModel.js')(sequelize, Sequelize);
+// db.UserHistory = require('./userHistoryModel.js')(sequelize, Sequelize);
 
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
+// Define User-Agents junction table
+const UserAgent = sequelize.define('userAgent', {});
+
+// Define Agents-ServiceProvider junction table
+const AgentServiceProvider = sequelize.define('agentServiceProvider', {});
+
+// Define User-ServiceProvider junction table
+const UserServiceProvider = sequelize.define('userServiceProvider', {
+  serviceNo: {
+    type: Sequelize.INTEGER
   }
 });
 
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
+// Define associations
+db.User.belongsToMany(db.Agents, {
+  through: UserAgent,
+  as: 'Agents',
+  foreignKey: 'UserId'
+});
+db.Agents.belongsToMany(db.User, {
+  through: UserAgent,
+  as: 'User',
+  foreignKey: 'AgentBIN'
+});
+
+db.Agents.belongsToMany(db.ServiceProviders, {
+  through: AgentServiceProvider,
+  as: 'ServiceProviders',
+  foreignKey: 'AgentBIN'
+});
+db.ServiceProviders.belongsToMany(db.Agents, {
+  through: AgentServiceProvider,
+  as: 'Agents',
+  foreignKey: 'serviceProviderBIN'
+});
+
+db.User.belongsToMany(db.ServiceProviders, {
+  through: UserServiceProvider,
+  as: 'ServiceProviders',
+  foreignKey: 'UserId'
+});
+db.ServiceProviders.belongsToMany(db.User, {
+  through: UserServiceProvider,
+  as: 'User',
+  foreignKey: 'serviceProviderBIN'
+});
+
+db.Agents.hasMany(db.Payment);
+db.Payment.belongsTo(db.Agents);
+
+db.User.hasMany(db.Payment);
+db.Payment.belongsTo(db.User);
+
+db.ServiceProviders.hasMany(db.Payment);
+db.Payment.belongsTo(db.ServiceProviders);
+
+db.ServiceProviders.hasMany(db.Bill);
+db.Bill.belongsTo(db.ServiceProviders);
+
+db.Payment.hasOne(db.Bill);
+db.Bill.belongsTo(db.Payment);
+
+db.User.hasMany(db.Bill);
+db.Bill.belongsTo(db.User);
+
+db.UserAgent = UserAgent;
+db.AgentServiceProvider = AgentServiceProvider;
+db.UserServiceProvider = UserServiceProvider;
 
 module.exports = db;
