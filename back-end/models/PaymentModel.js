283e60d545d31937b796edf7ead5cc5b064270ff
mahianbes12 <88880755+mahianbes12@users.noreply.github.com>
2023-07-21 01:15:57 -0400

    Payment Moodel

--- back-end/models/PaymentModel.js
+++ back-end/models/PaymentModel.js
@@ -1,42 +1,60 @@
-module.exports = (sequelize, DataTypes) => {
-    const payment = sequelize.define("payment", {
-        paymentID: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        paymentDate: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        status: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        amount: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-        },
-        payerID: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        payeeID: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        paymentMethod: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        paymentDescription: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
+const { ServiceProviders, Bill, User, Agents } = require(".");
 
-        receiptNumber: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
+module.exports = (sequelize, DataTypes) => {
+  const Payment = sequelize.define("Payment", {
+    TransactionNo: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    paymentDate: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    amount: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    payerID: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    payeeID: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    paymentMethod: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    paymentDescription: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    ReferenceNo: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+
+  Payment.associate = (models) => {
+    Payment.belongsTo(models.Agents, {
+      foreignKey: 'agentBIN',
+      as: 'Agents',
     });
-    return payment;
+    Payment.belongsTo(models.User, {
+      foreignKey: 'UserId',
+      as: 'Payer',
+    });
+    Payment.belongsTo(models.ServiceProviders, {
+      foreignKey: 'serviceProviderBIN',
+      as: 'Payee',
+    });
+    Payment.belongsTo(models.Bill, {
+      foreignKey: 'paymentId',
+      as: 'Bills',
+    });
+  };
+
+  return Payment;
 };
